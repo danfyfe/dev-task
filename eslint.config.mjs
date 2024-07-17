ea9aa799d5db30b04e7f0af0673c01ac7d09e4c9
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-17 13:43:36 -0400

    remove prettier from lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import eslintPluginPrettier from "eslint-plugin-prettier";
 
 
 export default [
@@ -11,6 +10,5 @@ export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
-  eslintPluginPrettier
+  pluginReactConfig
 ];
